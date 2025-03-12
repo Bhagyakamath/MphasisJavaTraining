aadfb25af519a8758504bcf431c74af19f9732b0
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-03-12 15:27:11 +0530

    Commit

---
+++ SpringCoreExp1/src/main/java/com.test/EmpConfig.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmpConfig {
+	
+	@Bean
+	public static Address getAdr1() {
+		return new Address();
+	}
+	
+	@Bean
+	public static Address getAdr2() {
+		return new Address("KA", "India", 456789);
+	}
+	
+	@Bean
+	public static Employee getEmp1() {
+		return new Employee();
+	}
+	
+	@Bean
+	public static Employee getEmp2() {
+		return new Employee(111, "Kohli", "ICC", "Bangalore", getAdr2());
+	}
+	
+	
+
+}
