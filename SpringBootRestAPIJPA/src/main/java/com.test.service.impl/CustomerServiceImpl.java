7d294d4a9da92d7440996bf8e7fc8feca1ce5b80
Bhagyakamath <120483354+Bhagyakamath@users.noreply.github.com>
2025-03-14 13:59:57 +0530

    commit

---
+++ SpringBootRestAPIJPA/src/main/java/com.test.service.impl/CustomerServiceImpl.java
@@ -0,0 +1,69 @@
+package com.test.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.test.dao.CustomerDao;
+import com.test.entity.Customer;
+import com.test.service.CustomerService;
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+	
+	@Autowired
+	private CustomerDao customerDao;
+	public CustomerDao getCustomerDao() {
+		return customerDao;
+	}
+
+	public void setCustomerDao(CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
+	
+
+	@Override
+	public Customer createCustomer(Customer cst) {
+		try {
+			if(cst!=null) {
+				customerDao.createCustomer(cst);
+			}
+		}
+		catch(RuntimeException e) {
+			e.printStackTrace();
+		}
+		
+		return cst;
+	}
+
+	@Override
+	public List<Customer> readAllCustomers() {
+		List<Customer> list=customerDao.readAllCustomer();
+		if(list.size()<0) {
+			System.out.println("no content");
+		}
+		return list;
+	}
+
+	@Override
+	public List<Customer> updateCustomer(Customer cst) {
+		try {
+			if(cst!=null) {
+				customerDao.updateCustomer(cst);
+			}
+		}
+		catch(RuntimeException e) {
+			e.printStackTrace();
+		}
+		
+		return customerDao.readAllCustomer();
+	}
+
+	@Override
+	public List<Customer> deleteCustomer(int id) {
+		
+		return customerDao.readAllCustomer();
+	}
+
+}
